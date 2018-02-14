989440ff27f289cd4ef3acdd07b0ce31eacb82ab
SERNA Verónica <Veronica.SERNA@aldautomotive.com>
2018-02-14 18:27:46 +0100

    CounterMore

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -2,12 +2,36 @@ import React, {Component} from 'react';
 
 
 class Counter extends Component{
+  constructor(props){
+    super(props);
+
+    this.state ={
+        counter: 0
+    };
+    onCounterMore(){
+      this.setState({
+        counter: this.state.counter +1
+      });
+
+    }
+    onCounterLess(){
+        this.setState({
+          counter: this.state.counter -1
+        });
+  
+      }
+
+
+  }
+
+
+
 render() {
 
     return (
     <div style={{margin: '0 auto'}}>
-    <button type="button" className="btn btn-secondary">Sumar</button>
-   <p style = {{margin: '10px auto'}}>0</p>
+    <button type="button" className="btn btn-secondary" onClick={this.onCounterMore()} >Sumar</button>
+   <p style = {{margin: '10px auto'}}>{this.state.counter}</p>
     <button type="button" className="btn btn-secondary">Restar</button>
 
    </div>
