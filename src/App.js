9d5d11e6e6d000928094587e14abb0dd7d646ff8
SERNA Verónica <Veronica.SERNA@aldautomotive.com>
2018-02-14 16:52:43 +0100

    App created with a Bootrap Card loading json data

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/card/Card';
+
+import Data from './data.json';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="row">
+
+        {Data.data.map(item => (
+          <Card
+          
+          name={item.name}
+          photo={item.photo}
+          description={item.description}
+          
+          
+          
+          />
+
+        ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Card = () => (
+
+<div className="ontainer">
+        <div className="col">
+
+            <div className="card" style={{width: '18rem', margin: '0 auto'}}>
+
+                <img className="img-fluid" src="" alt="card-foto"></img>
+                <div className="card-body">
+                <h4 className="card-title">Título</h4>
+                </div>
+                
+
+                <p className="card-text">Descripción</p>
+
+
+
+            </div>
+
+
+
+        </div>
+        
+      
+        </div>    
+
+       
+);
+export default Card;
